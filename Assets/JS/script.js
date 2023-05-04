9973626d784e223701ea232f54311800886fd303
ElenaLoyola <elenaloyola.a@gmail.com>
2023-05-04 16:07:52 -0400

    first commit

---
+++ Assets/JS/script.js
@@ -0,0 +1,12 @@
+function desplegarChats() {
+    let chatsRecientes = document.querySelector('.sidebar');
+    chatsRecientes.classList.toggle("mostrar");
+};
+function quitarNegrita() {
+    let chatNoLeido = document.getElementById("noLeido");
+    chatNoLeido.style.fontWeight="normal";
+};
+function quitarNegrita2() {
+    let chatNoLeido2 = document.getElementById("noLeido2");
+    chatNoLeido2.style.fontWeight="normal";
+};
